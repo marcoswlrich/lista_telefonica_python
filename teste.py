00fc1774e213d566ed627f68751b3543acb79337
Marcos Wlrich <marcoswlrich@gmail.com>
2020-02-25 23:33:05 -0300

    Add primeiras classes

---
+++ teste.py
@@ -0,0 +1,17 @@
+from lista_unica import ListaUnica
+from nome import Nome
+#Teste de classes do lista_unica
+lu = ListaUnica(int)
+lu.adiciona(5)
+lu.adiciona(3)
+lu.adiciona(10)
+# print(lu.lista[0])
+
+a = Nome('Marcos')
+print(a)
+#b = Nome('   ') #para teste de nomes nulos
+a = Nome("Marcos")
+print(a)
+
+
+
