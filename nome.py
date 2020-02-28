07f2120882f3b1701c131da8de0870f0410c67bb
Marcos Wlrich <marcoswlrich@gmail.com>
2020-02-27 23:09:27 -0300

    Atualização de classes

--- nome.py
+++ nome.py
@@ -1,5 +1,5 @@
 from functools import total_ordering
-
+from lista_unica import ListaUnica
 @total_ordering
 class Tipo_Telefone:
     def __init__(self, tipo):
@@ -66,3 +66,50 @@ class Nome:
     @staticmethod
     def Cria_chave(nome):
         return nome.strip().lower()
+
+class Telefones(ListaUnica):
+    def __init__(self):
+        super().__init__(Telefone)
+
+class Dado_agenda:
+    def __init__(self, nome):
+        self.nome = nome
+        self.telefones = Telefones()
+
+    @property
+    def nome(self):
+        return self.__nome
+    @nome.setter
+    def nome(self, valor):
+        if type(valor)!=Nome:
+            raise TypeError ('nome deve ser uma instância da classe Nome')
+        self.__nome = valor
+    def pesquisa_telefone(self, telefone):
+        posicao = self.telefones.pesquisa(Telefone(telefone))
+        if posicao == -1:
+            return None
+        else:
+            return self.telefones[posicao]
+
+class Tipos_telefone(ListaUnica):
+    def __init__(self):
+        super().__init__(Tipo_Telefone)
+
+class Agenda(ListaUnica):
+    def __init__(self):
+        super().__init__(Dado_agenda)
+        self.tipos_telefone = Tipos_telefone()
+    def adiciona_tipo(self, tipo):
+        self.tipos_telefone.adiciona(Tipo_Telefone(tipo))
+    def pesquisa_nome(self, nome):
+        if type(nome) == str:
+            nome = Nome(nome)
+        for dados in self.lista:
+            if dados.nome == nome:
+                return dados
+
+        else:
+            return None
+    def ordena(self):
+        super().ordena(lambda dado: str(dado.nome))
+
