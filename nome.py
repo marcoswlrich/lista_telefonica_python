06e4e19a2a58409a9b1ba3f5db7a91df7b930e1e
Marcos Wlrich <marcoswlrich@gmail.com>
2020-02-27 22:15:52 -0300

    add métodos

--- nome.py
+++ nome.py
@@ -1,17 +1,30 @@
+from functools import total_ordering
+
+@total_ordering
 class Nome:
     def __init__(self, nome):
-        if nome == None or not nome.strip():
-            raise ValueError('Nome não pode ser nulo nem em branco')
         self.nome = nome
-        self.chave = nome.strip().lower()
-
     def __str__(self):
         return self.nome
     def __repr__(self):
         return "<Classe {3} em 0x{0:x} Nome: {1} Chave: {2}>" .format(id(self), self.nome, self.chave, type(self).__name__)
     def __eq__(self, outro):
-        print("__eq__ Chamado")
         return self.nome == outro.nome
     def __lt__(self, outro):
-        print("__lt__ Chamado")
         return self.nome < outro.nome
+
+    @property
+    def nome(self):
+        return  self.__nome
+    @nome.setter
+    def nome(self, valor):
+        if valor == None or not valor.strip():
+            raise ValueError('Nome não pode ser nulo nem em branco')
+        self.__nome = valor
+        self.__chave = Nome.Cria_chave(valor)
+    @property
+    def chave(self):
+        return self.__chave
+    @staticmethod
+    def Cria_chave(nome):
+        return nome.strip().lower()
