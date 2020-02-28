f17de0ce8c6080e489a2971062bfccd0e55e4d75
Marcos Wlrich <marcoswlrich@gmail.com>
2020-02-27 22:41:15 -0300

    Add classes Tipo_Telefone e Telefone

--- nome.py
+++ nome.py
@@ -1,6 +1,44 @@
 from functools import total_ordering
 
 @total_ordering
+class Tipo_Telefone:
+    def __init__(self, tipo):
+        self.tipo = tipo
+    def __str__(self):
+        return '({0})' .format(self.tipo)
+    def __eq__(self, outro):
+        if outro is None:
+            return False
+        return self.tipo == outro.tipo
+    def __lt__(self, outro):
+        return self.tipo < outro.tipo
+
+class Telefone:
+    def __init__(self, numero, tipo = None):
+        self.numero = numero
+        self.tipo = tipo
+    def __str__(self):
+        if self.tipo != None:
+            tipo = self.tipo
+        else:
+            tipo = ''
+        return '{0} {1}' .format(self.numero, tipo)
+    def __eq__(self, outro):
+        return self.numero ==outro.numero and ((self.tipo == outro.tipo) or (self.tipo == None or outro.tipo == None))
+
+    @property
+    def numero(self):
+        return self.__numero
+    @numero.setter
+    def numero(self, valor):
+        if valor == None or not valor.strip():
+            raise ValueError("Número não pode ser None ou em branco")
+        self.__numero = valor
+
+
+
+
+@total_ordering
 class Nome:
     def __init__(self, nome):
         self.nome = nome
